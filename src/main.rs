6b2b6c320d08e4a21279e8f943875bbb04c2c98f
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-05-26 03:36:00 -0400

    create a file searching function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::fs::File;
+use std::path::Path;
+use std::io::prelude::*;
+
+fn search_for_file(filename: String) -> Option<&Path> {
+    /*
+    Searches for a filepath, and if found returns the Path object
+    if not found, returns false
+    */
+    let filepath: &Path = Path::new("file_directory/{filename}");
+    let result: bool = filepath.try_exists().unwrap();
+    if result {
+        return Some(filepath);
+    }
+    None
 }
+
+fn write_to_file() -> std::io::Result<()> {
+    let mut file = File::create("file_directory/foo.txt")?;
+    file.write_all(b"Hello, world!")?;
+    Ok(())
+}
+
+fn main() {
+    
+}
+
+
+
+
+
+// simplest key value store database ever 
+// find a file, if it doesn't exist or the current file is full, create one
+// write a key and value store to the file, then search for it and print it out
