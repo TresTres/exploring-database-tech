8acfe3d9e75388e618ec6d5ba2966b772a63f1fa
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-05-26 04:12:51 -0400

    add an ability to append to files

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-
-use std::fs::File;
-use std::path::Path;
+use std::fs::{OpenOptions};
+use std::path::PathBuf;
 use std::io::prelude::*;
 
-fn search_for_file(filename: String) -> Option<&Path> {
+fn search_for_file(filename: &str) -> Option<PathBuf> {
     /*
     Searches for a filepath, and if found returns the Path object
     if not found, returns false
     */
-    let filepath: &Path = Path::new("file_directory/{filename}");
-    let result: bool = filepath.try_exists().unwrap();
+    let filepath = PathBuf::from(&format!("file_directory/{}", filename));
+    let result = filepath.try_exists().unwrap();
     if result {
         return Some(filepath);
     }
     None
 }
 
-fn write_to_file() -> std::io::Result<()> {
-    let mut file = File::create("file_directory/foo.txt")?;
-    file.write_all(b"Hello, world!")?;
-    Ok(())
-}
-
 fn main() {
-    
+    let filename = "foo.txt";
+    let path = search_for_file(filename);
+    let mut open_options = OpenOptions::new();
+    open_options.create(true).append(true).read(true);
+    let mut file; 
+    match path {
+        Some(path) => {
+            println!("File found: {:?}", path);
+            file = open_options.open(path).unwrap();
+        }
+        None => {
+            println!("File not found, creating file...");
+            file = open_options.open(&format!("file_directory/{}", filename)).unwrap();
+        }
+    }
+    writeln!(file, "key: value").expect("Failed to write to file");
 }
 
-
-
-
-
-// simplest key value store database ever 
+// simplest key value store database ever
 // find a file, if it doesn't exist or the current file is full, create one
 // write a key and value store to the file, then search for it and print it out
