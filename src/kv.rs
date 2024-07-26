3fcf17e168200c7d460458c6ec34fe883ff41153
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-07-25 23:24:29 -0400

    starting implementation of kv store

---
+++ src/kv.rs
@@ -0,0 +1,37 @@
+
+use crate::common::file;
+
+
+
+pub struct KVStore {
+    filename: String,
+    target_dir: String,
+    file_length: u64,
+    buffer: Vec<u8>,
+}
+
+
+impl KVStore {
+    pub fn new(filename: &str, target_dir: &str) -> Self {
+        KVStore {
+            filename: filename.to_string(),
+            target_dir: target_dir.to_string(),
+            file_length: 100, 
+            buffer: Vec::new(),
+        }
+    }
+}
+
+
+pub fn write_value_to_store(store: KVStore, key: &str, value: &str) -> Result<
+    bool, 
+    std::io::Error
+>{
+    file::write_to_file(store.filename.as_str(), store.target_dir.as_str());
+    Ok(true)
+}
+
+
+
+#[cfg(test)]
+mod tests;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod common;
-// simplest key value store database ever
-// find a file, if it doesn't exist or the current file is full, create one
-// write a key and value store to the file, then search for it and print it out
+mod kv;
 
 fn main() {
     let filename = "foo.txt";
     let target_dir = "file_directory";
-    common::file::write_to_file(filename, target_dir);
+    let store = kv::KVStore::new(filename, target_dir);
+    kv::write_value_to_store(store, "foo", "bar");
 }
 
 
