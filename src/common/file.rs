1a4e86fb3e9d451fad83aac83929c32176ce6ea7
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2024-07-26 12:57:04 -0500

    POC of reading key from file

--- src/common/file.rs
+++ src/common/file.rs
@@ -1,11 +1,12 @@
 
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::fs::OpenOptions;
 use std::path::PathBuf;
 
 
 
-pub fn write_line_to_file(filepath: PathBuf, line: &str) -> Result<
+pub fn write_line_to_file(filepath: &PathBuf, line: &str) -> Result<
     (),
     std::io::Error
 > {
@@ -24,6 +25,13 @@ pub fn write_line_to_file(filepath: PathBuf, line: &str) -> Result<
     Ok(())
 }
 
+pub fn read_file_to_buffer(filepath: &PathBuf) -> BufReader<std::fs::File> {
+    /*
+    Reads a file to a buffer.  If a file does not exist, it returns an error.
+    */
+    let file = OpenOptions::new().read(true).open(filepath).expect("Failed to open file");
+    BufReader::new(file)
+}
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ mod kv;
 fn main() {
     let filename = "foo.txt";
     let target_dir = "file_directory";
-    let store = kv::KVStore::new(filename, target_dir);
-    kv::write_value_to_store(store, "foo", "bar").expect("Failed to write to store");
+    let mut store = kv::KVStore::new(filename, target_dir);
+    kv::write_value_to_store(&store, "foo", "batz").expect("Failed to write to store");
+    store.load().expect("Failed to load store");
+    println!("{:?}", store.get_key("foo"));
 }
 
 
